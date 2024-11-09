9de458157b54947fd57d23c42e09f12403d82e50
sergiolopez <sergio.lopez.souto@gmail.com>
2024-11-09 22:37:06 +0100

    adding page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <p> PORTFOLIO</p>;
+export default function Page() {
+  return (
+    <main>
+      <section id="hero"></section>
+
+      <section id="work"></section>
+
+      <section id="education"></section>
+
+      <section id="skills"></section>
+
+      <section id="projects"></section>
+    </main>
+  );
 }
