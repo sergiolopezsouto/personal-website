067ed20938e1aee500b993edf60140156ccd8736
sergiolopez <sergio.lopez.souto@gmail.com>
2024-11-11 17:12:56 +0100

    adding contact section (call to action)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { DATA } from "@/data/data";
+import Link from "next/link";
 import BlurFade from "@/components/magicui/blur-fade";
 import BlurFadeText from "@/components/magicui/blur-fade-text";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -112,8 +113,41 @@ export default function Page() {
         </div>
       </section>
 
-      <section id="projects">
+      {/* <section id="projects">
         <h5>PROJECTS</h5>
+      </section> */}
+
+      <section id="contact">
+        <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full pt-28 pb-36">
+          <BlurFade delay={BLUR_FADE_DELAY * 16}>
+            <div className="space-y-3">
+              {/* <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                Contact
+              </div> */}
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                Get in Touch
+              </h2>
+              <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Want to chat? Just shoot me an{" "}
+                <Link
+                  href={DATA.contact.Gmail.url}
+                  className="text-blue-500 hover:underline"
+                >
+                  email
+                </Link>{" "}
+                or connect with me on{" "}
+                <Link
+                  href={DATA.contact.LinkedIn.url}
+                  className="text-blue-500 hover:underline"
+                >
+                  Linkedin
+                </Link>
+                {". "}
+                I&apos;ll respond as soon as possible.
+              </p>
+            </div>
+          </BlurFade>
+        </div>
       </section>
     </main>
   );
